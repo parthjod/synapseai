15d36a0d06aea677d896870f0e8db9adca045662
parthjod <hlomello765@gmail.com>
2025-03-27 00:32:10 +0530

    updated links

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,13 +5,13 @@ function Footer() {
     <footer className="footer">
       <p>© 2025 Synapse AI Club</p>
       <div className="social-icons">
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/company/synapse-ai-club/" target="_blank">
           <i className="fab fa-linkedin"></i>
         </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://twitter.com/synapse-ai-club/" target="_blank">
           <i className="fab fa-twitter"></i>
         </a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.instagram.com/synapse_mitwpu/" target="_blank">
           <i className="fab fa-instagram"></i>
         </a>
       </div>
